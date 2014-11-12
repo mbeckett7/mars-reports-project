20f1344ed2c910d2eac18e7a7f2311a547cde1a0
Vernica Downey <vmdowney@gmail.com>
2014-11-12 13:18:36 -0500

    Add filters for music and R04 reports
    Added code to place music headings in separate reports for R00, R06,
    R07, and R11 and to place 'No Replacement Found' R04 headings in a
    separate report. Fixed encoding issue.

--- mars_enhance_csv.py
+++ mars_enhance_csv.py
@@ -3,6 +3,7 @@
 Script for enhancing MARS reports with data from the HOLLIS Presto API and the MARS transactions reports.
 Created for the Harvard Library ITS MARS Reports Pilot Project, 2014.
 '''
+import codecs
 import csv
 import glob
 import requests
@@ -10,6 +11,11 @@ import time
 from lxml import html
 
 bib_dict = {} # Dictionary of HOLLIS bib numbers
+enhanced_dict = {} # Dictionary of enhanced data
+music_reports = ['R00','R06','R07','R11'] # List of reports to check for music headings
+no_replace_reports = ['R04'] # List of reports to check for 'No Replacement Found' records
+no_enhance_reports = ['R03', 'R04'] # List of reports that cannot or will not be enhanced
+# Authority reports without bib numbers cannot be enhanced by the HOLLIS Presto API
 
 # Get bib numbers from all of the current report CSV files
 for file in glob.glob('*.csv'):
@@ -40,49 +46,77 @@ for bib, fields in bib_dict.items(): # bib, fields as key, value
     presto = requests.get(marc_url)
     marc_xml = presto.content.replace('<record xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim   http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">','<record>')
     marc_record = html.fromstring(marc_xml)
-    ldr06 = marc_record.xpath('//leader/text()')[0][6] #Get LDR byte 06 (type of record)
-    language = marc_record.xpath('//controlfield[@tag="008"]/text()')[0][35:38] #Get 008 bytes 35-37 (language code)
-    own = marc_record.xpath('//datafield[@tag="OWN"]/subfield/text()') #Get list of OWN (holding library) fields
-    collection = marc_record.xpath('//datafield[@tag="852"]/subfield[@code="c"]/text()') #Get collection code from 852 $c
-    for (i, j) in zip(own, collection): #Combine own field and collection code and format as a text string
+    ldr06 = marc_record.xpath('//leader/text()')[0][6] # Get LDR byte 06 (type of record)
+    language = marc_record.xpath('//controlfield[@tag="008"]/text()')[0][35:38] # Get 008 bytes 35-37 (language code)
+    own = marc_record.xpath('//datafield[@tag="OWN"]/subfield/text()') # Get list of OWN (holding library) fields
+    collection = marc_record.xpath('//datafield[@tag="852"]/subfield[@code="c"]/text()') # Get collection code from 852 $c
+    for (i, j) in zip(own, collection): # Combine own field and collection code and format as a text string
         libraries.append( i + ' (' + j + ')')
     libraries = '; '.join(libraries)
-    bib_dict[bib] = [ldr06, language, libraries]
-    if index % 25 == 0: # Delay to avoid being locked out of API
-        time.sleep(2)
-    else:
-        time.sleep(1)
+    bib_dict[bib] = [ldr06, language, libraries] # Add HOLLIS data to dictionary of bib numbers
+    time.sleep(1)
 
-
-enhanced_dict = {} # Dictionary of enhanced data
-
-# Add data to CSV files
+# Add HOLLIS data to CSV files
 for file in glob.glob('*.csv'):
     with open(file, 'rb') as mars_csv:
         reader = csv.reader(mars_csv)
 
         enhanced_rows = []
-        enhanced_file = file[:-4] + '_enhanced.csv'
+        music_rows = [] # For music reports
+        no_replace_rows = [] # For 'No Replacement Found' R04 report
+        report_no = file[:4].replace('_','')
 
-        for index, row in enumerate(reader):
-            if index == 0:
-                row[:-3] += ['LDR 06','Language','Libraries']
-            elif row[0] in bib_dict:
-                row[:-3] += bib_dict[row[0]]
-            elif row[1] in bib_dict:
-                row[:-3] += bib_dict[row[1]]
-            else:
-                row[:-3] += ['','','']
-            enhanced_rows.append(row)
-        enhanced_dict[enhanced_file] = enhanced_rows
+        if report_no not in no_enhance_reports: # Only enhance reports that can be enhanced
+            for index, row in enumerate(reader):
+                if index == 0: # Get header row
+                    row[0] = row[0].replace('"', '')
+                    row[:-3] += ['LDR 06','Language','Libraries']
+                    enhanced_rows.append(row)
+                elif row[0] in bib_dict and bib_dict[row[0]] != None: # Check first column
+                    row[:-3] += bib_dict[row[0]]
+                    if report_no in music_reports: # For music reports, check for LDR 06 c, d, or j 
+                        if bib_dict[row[0]][0] == 'c' or bib_dict[row[0]][0] == 'd' or bib_dict[row[0]][0] == 'j': 
+                            music_rows.append(row) # Put in music report
+                        else:
+                            enhanced_rows.append(row) # Put in non-music report
+                    else:
+                        enhanced_rows.append(row)
+                elif row[1] in bib_dict and bib_dict[row[1]] != None: # Check second column
+                    row[:-3] += bib_dict[row[1]]
+                    if report_no in music_reports: # For music reports, check for LDR 06 c, d, or j
+                        if bib_dict[row[1]][0] == 'c' or bib_dict[row[1]][0] == 'd' or bib_dict[row[1]][0] == 'j':
+                            music_rows.append(row) # Put in music report
+                        else:
+                            enhanced_rows.append(row) # Put in non-music report
+                else:
+                    row[:-3] += ['','','']
+                    enhanced_rows.append(row)
+        elif report_no in no_replace_reports:
+            for index, row in enumerate(reader):
+                if index == 0: # Get header row
+                    row[0] = row[0].replace('"', '')
+                    enhanced_rows.append(row)
+                else:
+                    if row[1] == 'No replacement found':
+                        no_replace_rows.append(row)
+                    else:
+                        enhanced_rows.append(row)
 
-# TO DO: Exclude reports without bib numbers (e.g., authority reports) from enhancement
-# TO DO: Split music records (LDR 06) into separate files
-# TO DO: Split 'No Replacement Found' (R04) into separate files
+        enhanced_dict[file[:-4] + '_enhanced.csv'] = enhanced_rows # Create enhanced report
 
+        if music_rows: # Create music report
+            music_rows.insert(0, enhanced_rows[0])
+            enhanced_dict[file[:-4] + '_enhanced_music.csv'] = music_rows
+
+        if no_replace_rows: # Create music report
+            no_replace_rows.insert(0, enhanced_rows[0])
+            enhanced_dict[file[:-4] + '_enhanced_noreplace.csv'] = no_replace_rows
+            # TO DO: Row numbers are not correct for separated reports; this should be fixed if it matters to the processing team
+            
+# Create CSV files for enhanced reports
 for csv_file, csv_data in enhanced_dict.items():
-    with open(csv_file, 'wb') as enhanced_csv:
-            writer = csv.writer(enhanced_csv)
+    with open(csv_file, 'wb') as output:
+            output.write(codecs.BOM_UTF8)
+            writer = csv.writer(output)
             writer.writerows(csv_data)
-            #TO DO: Encoding is not correct for enhanced files; to be fixed
     print csv_file, 'has been created'
